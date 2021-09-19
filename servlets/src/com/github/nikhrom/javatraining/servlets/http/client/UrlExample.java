8ff1416af53091f769685da1f31ece68ccbced2c
Никита Хромов <nikhrom2001@gmail.com>
2021-09-19 21:40:49 +0300

    Lessons 11-13 Http.Servlets dmdev
    basic knowledge of working with URL and HttpClient

---
+++ servlets/src/com/github/nikhrom/javatraining/servlets/http/client/UrlExample.java
@@ -0,0 +1,18 @@
+package com.github.nikhrom.javatraining.servlets.http.client;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class UrlExample {
+
+    public static void main(String[] args) throws IOException {
+        var url = new URL("https://www.google.com");
+        var urlConnection = url.openConnection();
+
+        urlConnection.getContent();
+
+    }
+
+}
