a593c124f479338be381d1ff3a2a284195e53159
Никита Хромов <nikhrom2001@gmail.com>
2021-11-16 15:53:26 +0300

    basic project structure created

---
+++ aop-introduction/src/main/java/github/nikhrom/javatraining/aop/introduction/Library.java
@@ -0,0 +1,9 @@
+package github.nikhrom.javatraining.aop.introduction;
+
+public class Library
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
