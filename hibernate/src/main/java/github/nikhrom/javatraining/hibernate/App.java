5ed7525d3cd847e6596e1773431beb9ab8251630
Никита Хромов <nikhrom2001@gmail.com>
2021-11-22 17:54:04 +0300

    basic project structure created for ZaurTregulov Spring course (chapter 3, Hibernate)

---
+++ hibernate/src/main/java/github/nikhrom/javatraining/hibernate/App.java
@@ -0,0 +1,13 @@
+package github.nikhrom.javatraining.hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
