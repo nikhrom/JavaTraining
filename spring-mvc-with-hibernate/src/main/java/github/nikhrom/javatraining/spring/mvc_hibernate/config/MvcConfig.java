f6f71accbb9196c87d85e8bbd8063ca5f8db97b4
Никита Хромов <nikhrom2001@gmail.com>
2021-12-13 22:39:18 +0300

    Created project for ZaurTregulov Spring (chapter 5. MVC+Hibernate+AOP) Lessons

---
+++ spring-mvc-with-hibernate/src/main/java/github/nikhrom/javatraining/spring/mvc_hibernate/config/MvcConfig.java
@@ -0,0 +1,86 @@
+package github.nikhrom.javatraining.spring.mvc_hibernate.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.thymeleaf.spring5.ISpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "github.nikhrom.javatraining.spring.mvc_hibernate")
+public class MvcConfig implements WebMvcConfigurer{
+
+    @Bean
+    public ThymeleafViewResolver viewResolver(ISpringTemplateEngine engine){
+        var resolver = new ThymeleafViewResolver();
+        resolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        resolver.setTemplateEngine(engine);
+        resolver.setOrder(1);
+        return resolver;
+    }
+
+
+    @Bean
+    public SpringTemplateEngine templateEngine(ITemplateResolver resolver,
+                                               MessageSource messageSource){
+        var engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(resolver);
+        engine.setEnableSpringELCompiler(true);
+        engine.setMessageSource(messageSource);
+        return engine;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver(){
+        var resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("/WEB-INF/template/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        resolver.setCacheable(true);
+        return resolver;
+    }
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource(){
+        var messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
+        messageSource.setBasename("classpath:messages");
+        return messageSource;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        var localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    };
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new CookieLocaleResolver();
+    }
+
+}
