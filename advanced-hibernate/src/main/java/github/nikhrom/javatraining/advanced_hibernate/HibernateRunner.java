f00f9110ce3904dba98a59c143dea03d88461459
Никита Хромов <nikhrom2001@gmail.com>
2022-02-11 16:17:29 +0300

    Dmdev Hibernate course (Chapter 13, DAO & Repository). Lesson 81 (second half)
    - TransactionInterceptor creating

--- advanced-hibernate/src/main/java/github/nikhrom/javatraining/advanced_hibernate/HibernateRunner.java
+++ advanced-hibernate/src/main/java/github/nikhrom/javatraining/advanced_hibernate/HibernateRunner.java
@@ -1,44 +1,36 @@
 package github.nikhrom.javatraining.advanced_hibernate;
 
 import github.nikhrom.javatraining.advanced_hibernate.dao.CompanyRepository;
-import github.nikhrom.javatraining.advanced_hibernate.dao.PaymentRepository;
 import github.nikhrom.javatraining.advanced_hibernate.dao.UserRepository;
 import github.nikhrom.javatraining.advanced_hibernate.dto.UserCreateDto;
-import github.nikhrom.javatraining.advanced_hibernate.entity.*;
+import github.nikhrom.javatraining.advanced_hibernate.entity.Birthday;
+import github.nikhrom.javatraining.advanced_hibernate.entity.PersonalData;
+import github.nikhrom.javatraining.advanced_hibernate.entity.Role;
 import github.nikhrom.javatraining.advanced_hibernate.interceptor.TransactionInterceptor;
 import github.nikhrom.javatraining.advanced_hibernate.mapper.CompanyReadMapper;
 import github.nikhrom.javatraining.advanced_hibernate.mapper.UserCreateMapper;
 import github.nikhrom.javatraining.advanced_hibernate.mapper.UserReadMapper;
 import github.nikhrom.javatraining.advanced_hibernate.service.UserService;
 import github.nikhrom.javatraining.advanced_hibernate.util.HibernateUtil;
-import github.nikhrom.javatraining.advanced_hibernate.util.TestDataImporter;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.internal.bytebuddy.ByteBuddy;
-import org.assertj.core.internal.bytebuddy.implementation.MethodDelegation;
-import org.assertj.core.internal.bytebuddy.matcher.ElementMatcher;
-import org.assertj.core.internal.bytebuddy.matcher.ElementMatchers;
-import org.hibernate.ReplicationMode;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.matcher.ElementMatchers;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.envers.AuditReader;
-import org.hibernate.envers.AuditReaderFactory;
 
-import javax.persistence.EntityManager;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Proxy;
 import java.time.LocalDate;
-import java.util.Date;
-import java.util.List;
 
 @Slf4j
 public class HibernateRunner {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
 
             var session = (Session) Proxy.newProxyInstance(Session.class.getClassLoader(), new Class[]{Session.class},
                     (proxy, method, args1) -> method.invoke(sessionFactory.getCurrentSession(), args1));
-
-            session.beginTransaction();
 
             var userRepository = new UserRepository(session);
             var companyRepository = new CompanyRepository(session);
@@ -47,14 +39,24 @@ public class HibernateRunner {
             var userReadMapper = new UserReadMapper(companyReadMapper);
             var userCreateMapper = new UserCreateMapper(companyRepository);
 
-            UserService userService = new UserService(userRepository, userReadMapper, userCreateMapper);
+            var transactionInterceptor = new TransactionInterceptor(sessionFactory);
+
+            UserService userService = new ByteBuddy()
+                    .subclass(UserService.class)
+                    .method(ElementMatchers.any())
+                    .intercept(MethodDelegation.to(transactionInterceptor))
+                    .make()
+                    .load(UserService.class.getClassLoader())
+                    .getLoaded()
+                    .getDeclaredConstructor(UserRepository.class, UserReadMapper.class, UserCreateMapper.class)
+                    .newInstance(userRepository, userReadMapper, userCreateMapper);
 
             userService.findById(1L).ifPresent(System.out::println);
 
             var userCreateDto = UserCreateDto.builder()
                     .companyId(1)
                     .role(Role.USER)
-                    .userName("nikhrom")
+                    .userName("nikhrom2")
                     .personalData(PersonalData.builder()
                             .firstname("Ivan")
                             .lastname("Ivanov")
@@ -65,8 +67,6 @@ public class HibernateRunner {
 
 
             userService.create(userCreateDto);
-
-            session.getTransaction().commit();
         }
     }
 
